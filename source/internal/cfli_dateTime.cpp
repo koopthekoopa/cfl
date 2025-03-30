b9ddd7232495f4823d7ca7bfb8b3175febf102e2
koopthekoopa <koopoffical@gmail.com>
2025-03-30 00:02:53 +0000

    Actually match CFLi_GetMonthOfDay

--- source/internal/cfli_dateTime.cpp
+++ source/internal/cfli_dateTime.cpp
@@ -1,7 +1,8 @@
 #include <CTRFaceLib.h>
 
-#define MONTH_OF_DAY_MAX    13
-static const s32 sMonthOfDay[MONTH_OF_DAY_MAX] = {
+#define MONTH_OF_DAY_FIRST   1
+#define MONTH_OF_DAY_LAST    12
+static const s32 sMonthOfDay[MONTH_OF_DAY_LAST+1] = {
     0,
     31,
     29,
@@ -18,7 +19,7 @@ static const s32 sMonthOfDay[MONTH_OF_DAY_MAX] = {
 };
 
 s32 CFLi_GetMonthOfDay(s32 month) {
-    if ((month-1) < (MONTH_OF_DAY_MAX-1)) {
+    if (month >= MONTH_OF_DAY_FIRST && month <= MONTH_OF_DAY_LAST) {
         return sMonthOfDay[month];
     }
     else {
